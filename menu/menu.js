4e6884af55ae7d2dda949ece7bd5dccba4967ecf
jagir73 <jasmingiroux@jasmingiroux.com>
2013-08-19 14:02:33 -0400

    First commit.

---
+++ menu/menu.js
@@ -0,0 +1,7 @@
+document.write('<p>IFT-2103 game projects');
+	document.write('<ul>');
+		document.write('<li><a href="ChickenTemple">Chicken Temple</a></li>');
+		document.write('<li><a href="WhackamStyle">Whackam Style</a></li>');
+		document.write('<li><a href="http://jonathanlacasse.com/ift-2103/ninja/">Ninja\'s Tale</a></li>');
+	document.write('</ul>');
+document.write('</p>');
